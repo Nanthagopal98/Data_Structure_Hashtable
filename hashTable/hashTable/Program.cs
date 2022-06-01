ea3beea1bbe8b4a17fb5fd64f60980428e512fec
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-01 15:48:30 +0530

    [Nantha] - check frequency added

---
+++ hashTable/hashTable/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+hashTable.ucMethods run = new hashTable.ucMethods();
+run.findFrequency();
---
+++ hashTable/hashTable/ucMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashTable
+{
+    internal class ucMethods
+    {
+        public void findFrequency()
+        {
+            string input = "to be or not to be";
+            string[] wordsArray = input.Split(' ');
+            IEnumerable<string> uniqueItems = wordsArray.Distinct<string>();
+            string [] words = uniqueItems.ToArray();
+            int[] frequency = new int[wordsArray.Length];
+            MyMapNode<int, string> hashtable = new MyMapNode<int, string>(wordsArray.Length);           
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < wordsArray.Length; j++)
+                {
+                    if (wordsArray[j].ToLower() == wordsArray[i].ToLower())
+                    {
+                        count++;
+                    }
+                }
+                frequency[i] = count;
+                hashtable.Add(i, wordsArray[i]);
+                
+                if (i < words.Length)
+                {
+                    Console.WriteLine(words[i] + " occurs " + count);                    
+                }
+                count = 0;
+            }
+
+        }
+    }
+}
