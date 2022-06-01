9e8480d8d0f03612cb1a57b949171214e2c0700e
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-01 16:00:48 +0530

    [Nantha] - Frequency For Paragraph Added

--- hashTable/hashTable/Program.cs
+++ hashTable/hashTable/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 hashTable.ucMethods run = new hashTable.ucMethods();
-run.findFrequency();
+Console.WriteLine("1 To FindFrequency");
+Console.WriteLine("2 To FindFrequency Paragraph");
+int select = Convert.ToInt32(Console.ReadLine());
+switch (select)
+{
+    case 1:
+        run.findFrequency();
+        break;
+    case 2:
+        run.findFrequencyParagraph();
+        break;
+}
+
--- hashTable/hashTable/ucMethods.cs
+++ hashTable/hashTable/ucMethods.cs
@@ -37,5 +37,34 @@ namespace hashTable
             }
 
         }
+        public void findFrequencyParagraph()
+        {
+            string input = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] wordsArray = input.Split(' ');
+            IEnumerable<string> uniqueItems = wordsArray.Distinct<string>();
+            string[] words = uniqueItems.ToArray();
+            int[] frequency = new int[wordsArray.Length];
+            MyMapNode<int, string> hashtable = new MyMapNode<int, string>(wordsArray.Length);
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < wordsArray.Length; j++)
+                {
+                    if (wordsArray[j].ToLower() == wordsArray[i].ToLower())
+                    {
+                        count++;
+                    }
+                }
+                frequency[i] = count;
+                hashtable.Add(i, wordsArray[i]);
+
+                if (i < words.Length)
+                {
+                    Console.WriteLine(words[i] + " occurs " + count);
+                }
+                count = 0;
+            }
+
+        }
     }
 }
